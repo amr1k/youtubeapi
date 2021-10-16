4bc0de7235e6674275445c0f4cbe2aa428479bed
amr1k <54102529+amr1k@users.noreply.github.com>
2021-10-16 13:49:49 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,23 +13,23 @@ from pubsub import Publish
 
 #declare global variables
 projectId = ''
-youtubeClientSecret = ''
-youtubeCredentials = ''
+youtubeClientSecretName = ''
+youtubeCredentialsName = ''
 topicId = ''
 scope = ["https://www.googleapis.com/auth/youtube.readonly"]
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
-rawClientSecret= None
-rawYoutubeCreds=None
+rawClientSecret = None
+rawYoutubeCreds = None
 
 try:
-  rawClientSecret = Secrets.access_secret_version(projectId, youtubeClientSecret , 'latest')
+  rawClientSecret = Secrets.access_secret_version(projectId, youtubeClientSecretName , 'latest')
 except:
   print('error loading youtube-client-secret from secret manager')
 
 try:
-  rawYoutubeCreds = Secrets.access_secret_version(projectId, youtubeCredentials, 'latest')
+  rawYoutubeCreds = Secrets.access_secret_version(projectId, youtubeCredentialsName, 'latest')
 except:
   print('error loading youtube-credential from secret manager')
 
@@ -44,22 +44,26 @@ def get_authenticated_service(clientSecret):
 
 # main method
 if __name__ == '__main__':
-  # - If there is not secret, the user has not authorised oAuth to call the API on behalf of the user
+  # - If there is no secret, the user has not authorised oAuth to call the API on behalf of the user
   # - Refresh the token using the client credentials downloaded and saved in the secrets manager
   # - Save the new token in the Google Cloud Secret Manager
   if(rawYoutubeCreds == None):
     print('Not authenticated, launching user authentication workflow')
     clientSecret = get_authenticated_service(rawClientSecret)
     print('saving client secret to secret manager')
-    resp = Secrets.add_secret_version(projectId, youtubeCredentials, clientSecret.to_json())
+    resp = Secrets.add_secret_version(projectId, youtubeCredentialsName, clientSecret.to_json())
     rawYoutubeCreds = clientSecret
 
   # Convert the content of the secret to Google oauth credentials object
-  print('convert youtube credentials from secrets manager object')
+  print('convert youtube credentials from secrets manager to Credentials object')
   youtubeCredentials = google.oauth2.credentials.Credentials.from_authorized_user_info(json.loads(rawYoutubeCreds), scopes=scope)  
 
-  # init the pubsub client
+  # init the pubsub class
   publishClient = Publish(projectId, topicId)
+
+  # Now that we have a key to authenticate build the youtube api endpoint to access the data based on the defined scope
+  youtube = build(API_SERVICE_NAME, API_VERSION, credentials = youtubeCredentials)
+
   # Repeatedly call the same api for monitoring purposes, sleep for 10 seconds.
   while(1):
     # if the token has been expired, refresh it and update the Google Cloud Secrets Manager
@@ -67,10 +71,9 @@ if __name__ == '__main__':
       print('refreshing youtube token')
       youtubeCredentials.refresh(Request())
       print('saving refresh token to secret manager')
-      Secrets.add_secret_version(projectId, youtubeCredentials, youtubeCredentials.to_json())
-
-    # Now that we have a key to authenticate build the youtube api endpoint to access the data based on the defined scope
-    youtube = build(API_SERVICE_NAME, API_VERSION, credentials = youtubeCredentials)
+      Secrets.add_secret_version(projectId, youtubeCredentialsName, youtubeCredentials.to_json())
+      # Regenerate the youtube endpoint with the new credentials
+      youtube = build(API_SERVICE_NAME, API_VERSION, credentials = youtubeCredentials)
 
     # Call a test api to ensure it is working
     channels_response = youtube.channels().list(
